a341a58c8143f992f7905fa1e47b8184bf3c9b87
gagansingh355 <75354427+gagansingh355@users.noreply.github.com>
2022-11-03 22:07:14 +0530

    Removing log line

--- code/parse.go
+++ code/parse.go
@@ -144,7 +144,6 @@ func parseCloudWatchLogs(request events.CloudwatchLogsEvent) []ingest.Log {
 		resourceValue = splitLogStream[0] + "-" + splitLogStream[1]
 		resoureProp[resourceProperty] = resourceValue
 	} else if strings.Contains(d.LogGroup, "/aws/fargate") {
-		fmt.Println("##################d.Owner = ", d.Owner)
 		resoureProp["system.aws.accountid"] = d.Owner
 		resoureProp["system.cloud.category"] = "AWS/LMAccount"
 	} else if strings.Contains(d.LogGroup, "/aws/cloudtrail") {

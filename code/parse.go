b71dc6fb75e6b12de753c017d29ce659d46d0595
Pooja Choudhary <pooja.choudhary@logicmonitor.com>
2023-03-02 14:05:48 +0530

    DEV-126037: Adding s3 resource mapping for cloudtrail logs

--- code/parse.go
+++ code/parse.go
@@ -223,9 +223,21 @@ func parseCloudTrailLogs(data events.CloudwatchLogsData) []ingest.Log {
 			}
 		} else if eventSource == "ecs.amazonaws.com" {
 			ecsStreamRegex, _ := regexp.Compile(`("cluster":"|"cluster": "|:cluster/)([^/][^,][^"]*)`)
+
 			ecsStreamArray := ecsStreamRegex.FindStringSubmatch(event.Message)
+
 			if len(ecsStreamArray) > 2 {
 				resoureIDMap["system.aws.arn"] = fmt.Sprintf("arn:aws:ecs:%s:%s:cluster/%s", awsRegion, data.Owner, ecsStreamArray[2])
+				accountLevelLog = false
+			}
+		} else if eventSource == "s3.amazonaws.com" {
+			s3Regex, _ := regexp.Compile(`("ARN":")(?P<arn>[^/][^,][^"]*)`)
+			s3RegexArray := s3Regex.FindStringSubmatch(event.Message)
+			s3Arn := s3Regex.SubexpIndex("arn")
+
+			if len(s3RegexArray) > 0 && s3Arn != 0 {
+				resoureIDMap["system.aws.arn"] = fmt.Sprintf(s3RegexArray[s3Arn])
+				fmt.Println("arn:aws:s3:%s:%s:arn/%s", awsRegion, data.Owner, s3RegexArray[s3Arn])
 				accountLevelLog = false
 			}
 		}

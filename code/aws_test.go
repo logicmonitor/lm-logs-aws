51ce1a87b7b28f0a4ec0e7e578e6a33b3fe9f455
choudharypooja <pooja.choudhary@logicmonitor.com>
2023-08-02 18:25:22 +0530

    DEV-138493:Adding test cases

--- code/aws_test.go
+++ code/aws_test.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"os"
+	"strings"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
@@ -9,4 +11,16 @@ import (
 func TestReturnEmptySecretValueOnEmptyInput(t *testing.T) {
 	secret := getSecretValue("")
 	assert.Equal(t, secret, "")
+}
+
+func TestAccessSecret(t *testing.T) {
+	t.Setenv("LM_ACCESS_KEY", "arn:aws:secretsmanager:region:key")
+	t.Setenv("LM_ACCESS_ID", "arn:aws:secretsmanager:region:id")
+	t.Setenv("USE_SECRET_MANAGER", "true")
+
+	useSecretManager := os.Getenv("USE_SECRET_MANAGER")
+	assert.Equal(t, "true", useSecretManager)
+	assert.True(t, strings.Contains(os.Getenv("LM_ACCESS_KEY"), "arn"))
+	assert.True(t, strings.Contains(os.Getenv("LM_ACCESS_ID"), "arn"))
+
 }

25a2fccc642c97f2babf872e271446101bac80be
Vishal Charpe <vishal.charpe@logicmonitor.com>
2024-09-02 18:39:43 +0530

    DEV-176765:  DEV-176765-aws-log-integration-support-aws-eks-logs-in-lm

--- code/parse.go
+++ code/parse.go
@@ -169,7 +169,7 @@ func parseCloudWatchLogs(request events.CloudwatchLogsEvent) []model.LogInput {
 		metadataMap = extractMetadata(awsRegion, "", "fargate.amazonaws.com")
 	} else if strings.Contains(d.LogGroup, "/aws/cloudtrail") {
 		return parseCloudTrailLogs(d)
-	} else if strings.Contains(d.LogGroup, "eks") || strings.Contains(d.LogGroup, "containerInsights") {
+	} else if strings.Contains(d.LogGroup, "/aws/eks") || strings.Contains(d.LogGroup, "/aws/containerInsights") {
 		resoureProp["system.aws.accountid"] = d.Owner
 		resoureProp["system.cloud.category"] = "AWS/LMAccount"
 		metadataMap = extractMetadata(awsRegion, "", "eks.amazonaws.com")
